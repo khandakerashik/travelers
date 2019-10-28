91a2bad61d0816e33bb1140d1c5b1045bccadc3e
Khandaker Md. Ashikul Hoque <kh.ashik96@gmail.com>
2019-10-28 14:18:53 +0600

    Add files via upload

--- models/user-model.js
+++ models/user-model.js
@@ -4,7 +4,7 @@ module.exports = {
 
 	getById: function(id, callback){
 
-			var sql = "select * from user where id=?";
+			var sql = "select * from users where id=?";
 			db.getResults(sql, [id], function(result){
 				if(result.length > 0 ){
 					callback(result[0]);
@@ -15,7 +15,7 @@ module.exports = {
 	},
 	getByemail:function(username, callback){
 
-			var sql = "select * from user where email=?";
+			var sql = "select * from users where email=?";
 			db.getResults(sql, [username], function(result){
 				if(result.length > 0 ){
 					callback(result[0]);
@@ -25,8 +25,8 @@ module.exports = {
 			});
 	},
 	validate: function(user, callback){
-		var sql ="select * from user where username=? and password=?";
-		db.getResults(sql, [user.username, user.password], function(result){
+		var sql ="select * from users where email=? and password=?";
+		db.getResults(sql, [user.inputEmail, user.inputPassword], function(result){
 
 			if(result.length > 0){
 				callback(true);
@@ -36,7 +36,7 @@ module.exports = {
 		});	
 	},
 	getAll: function(callback){
-		var sql = "select * from user";
+		var sql = "select * from users";
 		
 		db.getResults(sql, [], function(results){
 			
@@ -49,20 +49,20 @@ module.exports = {
 	},
 	insert: function(user, callback){
 
-		var sql ="insert into user values('', ?, ?,?,?,?,?)";
+		var sql ="insert into users values('', ?, ?,?,?,?,?)";
 		db.execute(sql, [user.fullname,user.username, user.password,user.mobile,user.gender,user.usertype], function(status){
 			callback(status);
 		});
 	},
 	update: function(user, callback){
-		var sql ="update user set username=?, password=? where id=?";
+		var sql ="update users set username=?, password=? where id=?";
 	
 		db.execute(sql, [user.username, user.password, user.id], function(status){
 			callback(status);
 		});
 	},
 	delete: function(id, callback){
-		var sql = "delete from user where id=?";
+		var sql = "delete from users where id=?";
 		db.execute(sql, [id], function(status){
 			callback(status);
 		});
