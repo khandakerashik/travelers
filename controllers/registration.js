c5afbd5637ab0505b94ff1ffdfd1764390c559fb
Farhan Tanvin <38113532+farhantanvin@users.noreply.github.com>
2019-10-31 00:00:59 +0600

    Add files via upload

--- controllers/registration.js
+++ controllers/registration.js
@@ -20,21 +20,69 @@ router.post('/freaks', function(request, response){
 		name: request.body.inputName,
 		email: request.body.inputEmail,
 		phone: request.body.inputPhone,
-		gender:  request.body.inputgender,
-		password:request.body.inputPasswordr,
-		profile_pic:"/abc"
+		gender:  request.body.inputGender,
+		password:request.body.inputPassword,
+		profile_pic:"/abc",
+		status:"1",
+		type:"freaks"
 	};
-	console.log(user.profile_pic);
+	
 
 	userModel.insertFreaks(user, function(status){	
-		if(status){
-			response.redirect("/login");
-		}else{
-			response.redirect("/registration/freaks");	
+		if(status)
+		{
+				userModel.insert(user, function(status){	
+ 			if(status){
+			 	response.redirect("/login");
+			}else{
+ 				response.redirect("/registration/freaks");	
+			     }
+ 		   });
+
 		}
 	});
 
+	
+
 });
+
+
+router.post('/agencies', function(request, response){
+	
+
+
+	var user = {
+		name: request.body.inputName,
+		email: request.body.inputEmail,
+		phone: request.body.inputPhone,
+		gender:  request.body.inputGender,
+		password:request.body.inputPassword,
+		profile_pic:"/abc",
+		status:"1",
+		type:"freaks"
+	};
+	
+
+	userModel.insertFreaks(user, function(status){	
+		if(status)
+		{
+				userModel.insert(user, function(status){	
+ 			if(status){
+			 	response.redirect("/login");
+			}else{
+ 				response.redirect("/registration/freaks");	
+			     }
+ 		   });
+
+		}
+	});
+
+	
+
+});
+
+
+
 
 module.exports = router;
 
