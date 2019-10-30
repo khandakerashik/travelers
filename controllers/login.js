9a872c159b1c867e83e056801701f6accf0df18b
Khandaker Md. Ashikul Hoque <kh.ashik96@gmail.com>
2019-10-31 00:37:11 +0600

    Add files via upload

--- controllers/login.js
+++ controllers/login.js
@@ -7,10 +7,20 @@ router.get('/', function(request, response){
 });
 
 router.post('/', function(request, response){
-	
-	
-			
-		
+	console.log(request.body.inputEmail);
+	var user = {
+		inputEmail: request.body.inputEmail,
+		inputPassword: request.body.inputPassword
+	};
+
+	userModel.validate(user, function(status){
+		if(status){
+			response.cookie('inputEmail', request.body.username);
+			response.redirect('/home');
+		}else{
+			response.send('invalid username/password');		
+		}
+	});
 
 });
 
