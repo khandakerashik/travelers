0647fe59c98a29847e57e760649be2c74880d960
Farhan Tanvin <38113532+farhantanvin@users.noreply.github.com>
2019-11-06 02:47:05 +0600

    Add files via upload

--- models/events-model.js
+++ models/events-model.js
@@ -2,61 +2,96 @@ var db = require('./db')
 
 module.exports = {
 
-	getAlleventsHome: function(callback){
-		var sql = "select * from events LIMIT 6";
-		db.getResults(sql, [], function(result){
-			if(result.length > 0 ){
-				callback(result);
-			}else{
-				callback([]);
-			}
-		});
-	},    
-	    
-	getAllevents: function(callback){
-		var sql = "select * from events ";
-		db.getResults(sql, [], function(result){
-			if(result.length > 0 ){
-				callback(result);
-			}else{
-				callback([]);
-			}
-		});
-	},
+getAlleventsHome: function(callback){
 
-	insertEvents: function(event, callback){
-		var sql ="insert into events values('',?,?,?,?,?,?,?,?,?,?,?,?)";
-		console.log(sql);
-		db.execute(sql, [event.tittle,event.postby,event.agencyname,event.place,event.date,event.duration,event.description,event.person,event.cost,event.image,event.category,event.status], function(status){
+			var sql = "select * from events LIMIT 6";
+			db.getResults(sql, [], function(result){
+				if(result.length > 0 ){
+					callback(result);
+				}else{
+					callback([]);
+				}
+			});
+	},
+    
+    
+getAllevents: function(callback){
+
+			var sql = "select * from events ";
+			db.getResults(sql, [], function(result){
+				if(result.length > 0 ){
+					callback(result);
+				}else{
+					callback([]);
+				}
+			});
+	},
+    
+    
+    
+ EventsCount: function(callback){
+
+			var sql = "SELECT COUNT(id) AS NI FROM events";
+			db.getResults(sql, [], function(result){
+				if(result.length > 0 ){
+					callback(result[0]);
+				}else{
+					callback([]);
+				}
+			});
+	},    
+    
+
+   getAlleventsByID: function(id,callback){
+
+			var sql = "select * from events where id =?";
+			db.getResults(sql, [id], function(result){
+				if(result.length > 0 ){
+					callback(result[0]);
+				}else{
+					callback([]);
+				}
+			});
+	},
+    
+    
+    
+    
+      insertBooking: function(booking, callback){
+
+		var sql ="insert into booking values('',?,?,?,?,?)";
+		db.execute(sql, [booking.eventid,booking.agencyname,booking.bookedby,booking.bookedby_name,booking.date], function(status){
 			callback(status);
 		});
-	},   
-	    
-	EventsCount: function(callback){
-
-		var sql = "SELECT COUNT(id) AS NI FROM events";
-		db.getResults(sql, [], function(result){
-			if(result.length > 0 ){
-				callback(result[0]);
-			}
-			else{
-				callback([]);
-			}
+	},
+    
+     update: function(user, callback){
+		var sql ="update events set person_capacity=?  where id=?";
+	
+		db.execute(sql, [user.capacity,user.id], function(status){
+			callback(status);
 		});
-	},    
-	    
+	},
+    
+    
+    
+    
+    AllBookingByemail:function(email,callback){
 
-	getAlleventsByID: function(id,callback){
-		var sql = "select * from events where id =?";
-		db.getResults(sql, [id], function(result){
-			if(result.length > 0 ){
-				callback(result[0]);
-			}else{
-				callback([]);
-			}
-		});
+			var sql = "select * from booking where bookedby=?";
+			db.getResults(sql, [email], function(result){
+				if(result.length > 0 ){
+					callback(result);
+				}else{
+					callback([]);
+				}
+			});
 	}
-          
+
+    
+    
+    
+    
 }
 
 
