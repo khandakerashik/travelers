58c072b5620397e59765be89513ac983f47c8141
arnab1632 <56104436+arnab1632@users.noreply.github.com>
2019-11-06 09:48:14 +0600

    Add files via upload

--- models/user-model.js
+++ models/user-model.js
@@ -24,17 +24,7 @@ module.exports = {
 			}
 		});
 },
-getAgencyByEmail: function(email, callback){
 
-	var sql = "select * from travel_agencies where email=?";
-	db.getResults(sql, [email], function(result){
-		if(result.length > 0 ){
-			callback(result[0]);
-		}else{
-			callback([]);
-		}
-	});
-},
 	getByemail:function(email, callback){
 
 			var sql = "select * from users where email=?";
@@ -167,15 +157,6 @@ getAgencyByEmail: function(email, callback){
 			});
 		},
 
-		insertEvents: function(add, callback){
-			var sql ="insert into events values('',?,?,?,?,?,?,?,?,?,?)";
-			console.log(sql);
-			db.execute(sql, [add.tittle,add.postby,add.agencyname,add.place,add.date,add.description,add.person,add.price,add.image,add.category], function(status){
-				callback(status);
-			});
-		},
-
-
 	update: function(user, callback){
 		var sql ="update users set username=?, password=? where id=?";
 	
@@ -268,6 +249,53 @@ alreadyHaveEmail: function(email, callback){
 					callback([]);
 				}
 			});
+	},
+
+
+
+
+
+
+//travel_agency
+
+	getAgencyByEmail: function(email, callback){
+
+		var sql = "select * from travel_agencies where email=?";
+		db.getResults(sql, [email], function(result){
+			if(result.length > 0 ){
+				callback(result[0]);
+			}else{
+				callback([]);
+			}
+		});
+	},
+
+	getByemailAgencies:function(email, callback){
+
+		var sql = "select * from travel_agencies where email=?";
+		db.getResults(sql, [email], function(result){
+			if(result.length > 0 ){
+				callback(result[0]);
+			}else{
+				callback([]);
+			}
+		});
+	},
+    
+    updateAgencies: function(user, callback){
+		var sql ="update travel_agencies set name=?, phone=?, password=?, profile_pic=? where email=?";
+	
+		db.execute(sql, [user.name, user.phone,user.password,user.profile_pic,user.email], function(status){
+			callback(status);
+		});
+	},
+    
+    updateUsers: function(user, callback){
+		var sql ="update users set name=?, password=? where email=?";
+	
+		db.execute(sql, [user.name,user.password,user.email], function(status){
+			callback(status);
+		});
 	}
      
     
@@ -275,6 +303,3 @@ alreadyHaveEmail: function(email, callback){
     
     
 }
-
-
-
